ebc92ca3ce25e0d58b0d117e5638ce762f218f22
David Husa <davidhusa@gmail.com>
2013-02-10 15:34:56 -0800

    initialize app with zurb, rspec suite and heroku-compatible production gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eventapp::Application.config.secret_token = '2f362fcb23ded245fd79323764fb81ba0a1c8db608423c9b4db482e0e47d0a3de0e013ec40c39675ec089c902896c8bf1e5dfd37315d93b6bb3ef0f4799b4638'
