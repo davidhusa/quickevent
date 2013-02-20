20fa5bc175cda45e42343c9f0a95f890ecbef59b
David Husa <davidhusa@gmail.com>
2013-02-19 20:55:50 -0800

    add pretty date helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def pretty_time(time)
+    time.strftime("%-l:%M %P")
+  end
+
+  def pretty_date(date)
+    date.strftime("%A, %B %e, %Y")
+  end
+
+  def pretty_quick_date(date, joiner = "/")
+    return_string = []
+    return_string << date.strftime("%-m")
+    return_string << date.strftime("%e")
+    return_string << date.strftime("%y")
+    
+    return_string.join(joiner)
+  end
 end
