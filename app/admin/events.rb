45808c809a106bd6dd2138afc06f2086a0a77bf0
David Husa <davidhusa@gmail.com>
2013-03-04 14:21:51 -0800

    Finish optimizing active admin interface

--- app/admin/events.rb
+++ app/admin/events.rb
@@ -1,3 +1,17 @@
 ActiveAdmin.register Event do
+
+  index do
+    column :title
+    column :start
+    column :end
+    column "Description", :short_description
+    column :twitter_hashtag
+    column :created_at
+    column :updated_at
+    column :id
+    default_actions
+  end
+
+
   
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ActiveRecord::Base
   attr_accessible :description, :end, :organizer, :start, :title, :twitter_hashtag
+
+  def short_description
+    maxlength = 32
+    (self.description[0...maxlength] || "") + (self.description.length > maxlength ? "..." : "")
+  end
 end
