e07bc8124969316f67939d8d69d30a307019acb1
David Husa <davidhusa@gmail.com>
2013-03-22 16:15:27 -0700

    Tweak admin screen colors

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,5 +1,17 @@
 class NewsController < ApplicationController
   def index
-    @news = News.order(:published_at).where("is_published = ?", true).reverse
+    raw_news = News.order(:published_at).where("is_published = ?", true).reverse || []
+
+    @news = Array.new
+    last_date = nil
+
+    raw_news.each do |item|
+      unless item.published_at.to_date == last_date
+        @news << item.published_at
+        last_date = item.published_at.to_date
+      end
+      @news << item
+    end
+
   end
 end
--- app/controllers/schedule_items_controller.rb
+++ app/controllers/schedule_items_controller.rb
@@ -1,5 +1,30 @@
 class ScheduleItemsController < ApplicationController
   def index
-    @schedule = ScheduleItem.order(:start)
+    raw_schedule = ScheduleItem.order(:start) || []
+
+    @schedule = Array.new
+    last_date = nil
+
+    raw_schedule.each do |item|
+      unless item.start.to_date == last_date
+        @schedule << item.start
+        last_date = item.start.to_date
+      end
+      @schedule << item
+    end
+
   end
 end
+
+    # raw_news = News.order(:published_at).where("is_published = ?", true).reverse || []
+
+    # @news = Array.new
+    # last_date = nil
+
+    # raw_news.each do |item|
+    #   unless item.published_at.to_date == last_date
+    #     @news << item.published_at
+    #     last_date = item.published_at.to_date
+    #   end
+    #   @news << item
+    # end
