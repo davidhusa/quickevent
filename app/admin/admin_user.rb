05344fca66558acde27383bc01e2271a40f67e8d
David Husa <davidhusa@gmail.com>
2013-03-29 15:41:37 -0700

    fix activeadmin root link and add event association to admin user tab

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,6 +1,7 @@
 ActiveAdmin.register AdminUser do     
   index do                            
-    column :email                     
+    column :email
+    column :event_id               
     column :current_sign_in_at        
     column :last_sign_in_at           
     column :sign_in_count             
@@ -13,7 +14,8 @@ ActiveAdmin.register AdminUser do
     f.inputs "Admin Details" do       
       f.input :email                  
       f.input :password               
-      f.input :password_confirmation  
+      f.input :password_confirmation
+      f.input :event
     end                               
     f.actions                         
   end                                 
